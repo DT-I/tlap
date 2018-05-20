ec6d5b47e8dac0e700e570c6f7479413a99548bc
Sean Thompson <sean@nonsenseless.net>
2018-05-20 09:30:08 -0500

    Add welcome index page.

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#show"
+  get "welcome/index"
 end
